98e03c892d54794d385d39ee3fad2cc4af2baf75
Raji <raghavraj2357@gmail.com>
2023-05-05 11:47:04 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "phs-40479",
-  url: "https://phs-40479.botics.co",
-  api: "https://phs-40479.botics.co/api/v1"
-}
+  "name": "phs-40479",
+  "url": "https://phs-40479.botics.co",
+  "api": "https://phs-40479.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/signup/index.js
@@ -0,0 +1,160 @@
+import { Text, View, TouchableOpacity, StyleSheet, TextInput, TouchableHighlight } from "react-native";
+
+const pressed = () => {
+  console.log("pressed");
+};
+
+const Signup = () => {
+  return <View style={styles.container}>
+      <View style={styles.heading}>
+        <Text style={styles.headingText}>Sign up</Text>
+      </View>
+      <View>
+        <View style={styles.emailContainer}>
+          <Text style={styles.mr10}>Email address</Text>
+          <Input placeholder="Email" />
+        </View>
+        <View style={styles.mb20}>
+          <Text style={styles.mr10}>Password</Text>
+          <Input placeholder="Enter" />
+        </View>
+        <View style={styles.mb20}>
+          <Text style={styles.mr10}>Confirm password</Text>
+          <Input placeholder="Enter" />
+        </View>
+
+        <View style={styles.loginContainer}>
+          <Button onPress={pressed}>Sign up</Button>
+        </View>
+        <View style={styles.orContainer}>
+          <View style={styles.line} />
+          <Text style={styles.orText}>Or</Text>
+          <View style={styles.line} />
+        </View>
+        
+      </View>
+      <View style={styles.footerContainer}>
+        <Text style={styles.footerText}>I have an account? </Text>
+        <TouchableOpacity onPress={pressed}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    height: "100%",
+    backgroundColor: "#fff"
+  },
+  heading: {
+    alignSelf: "center"
+  },
+  headingText: {
+    fontSize: 42,
+    fontWeight: "500"
+  },
+  emailContainer: {
+    marginBottom: 10
+  },
+  mr10: {
+    marginRight: 10,
+    marginBottom: 10
+  },
+  mb20: {
+    marginBottom: 20
+  },
+  loginContainer: {
+    width: "80%",
+    alignSelf: "center"
+  },
+  orContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: 222,
+    alignSelf: "center",
+    height: 69
+  },
+  line: {
+    height: 1,
+    width: 100,
+    backgroundColor: "rgba(0, 0, 0, 0.05)"
+  },
+  orText: {
+    marginVertical: 40,
+    alignSelf: "center",
+    fontSize: 16,
+    color: "#231F20",
+    opacity: 0.5
+  },
+  footerContainer: {
+    alignSelf: "center",
+    display: "flex",
+    flexDirection: "row"
+  },
+  footerText: {
+    color: "#6B6B6B"
+  }
+});
+export default Signup;
+
+const Button = props => {
+  return <TouchableHighlight onPress={props.onPress} underlayColor="#DDDDDD">
+      <View style={[btnStyles.button, {
+      backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
+      height: props.height ? props.height : 49,
+      borderWidth: props.borderWidth ? props.borderWidth : 0,
+      borderColor: props.borderColor ? props.borderColor : "#000000"
+    }]}>
+        <Text style={[btnStyles.text, {
+        color: props.color ? props.color : "#ffffff"
+      }]}>
+          {props.children}
+        </Text>
+      </View>
+    </TouchableHighlight>;
+};
+
+const btnStyles = StyleSheet.create({
+  button: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 15
+  }
+});
+
+const Input = props => {
+  return <View>
+      <TextInput style={textStyles.input} placeholder={props.placeholder} value={props.value} onChangeText={num => props.setValue(num)} placeholderTextColor="#ddd" editable={props.editable !== false} />
+      {props.errorText ? <Text style={textStyles.error}>{props.errorText}</Text> : null}
+    </View>;
+};
+
+const textStyles = StyleSheet.create({
+  input: {
+    backgroundColor: "#fff",
+    height: 53,
+    borderColor: "#C4C4C4",
+    color: "#000",
+    borderRadius: 10,
+    fontSize: 14,
+    borderWidth: 1,
+    paddingHorizontal: 10
+  },
+  error: {
+    fontSize: 13,
+    color: "#FA060D",
+    paddingTop: 8
+  }
+});
